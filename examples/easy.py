4cdc5514c9d5add32ba3898b6f2620043c214153
Grady O'Connell <flipcoder@gmail.com>
2020-03-20 23:32:41 -0700

    better example program

--- examples/easy.py
+++ examples/easy.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env qork
-
 data_path('../data')
 
-camera.pos = vec3(0,2,5)
+camera.pos = (0,0,5)
 player = add('player.png')
-player.velocity = -Y
+camera.velocity = -Z
 
 def update(t):
-    pass
+    print(player.position)
 
